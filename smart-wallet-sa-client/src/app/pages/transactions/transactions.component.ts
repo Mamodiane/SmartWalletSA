c9d27cc7a3db1e9dc18095317801019839e074c7
Pilatos <mamodianepilatos@gmail.com>
2026-05-13 23:45:35 +0200

    Add transaction filters and summary cards

--- smart-wallet-sa-client/src/app/pages/transactions/transactions.component.ts
+++ smart-wallet-sa-client/src/app/pages/transactions/transactions.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TransactionService } from '../../services/transaction.service';
 import { Transaction } from '../../models/transaction.model';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
 export class TransactionsComponent implements OnInit {
   transactions: Transaction[] = [];
+
+  selectedType = 'All';
   errorMessage = '';
 
   constructor(private transactionService: TransactionService) {}
@@ -29,5 +32,41 @@ export class TransactionsComponent implements OnInit {
         this.errorMessage = 'Unable to load transactions.';
       }
     });
+  }
+
+  get filteredTransactions(): Transaction[] {
+    if (this.selectedType === 'All') {
+      return this.transactions;
+    }
+
+    return this.transactions.filter(
+      transaction => transaction.type === this.selectedType
+    );
+  }
+
+  get totalDeposits(): number {
+    return this.transactions
+      .filter(t => t.type === 'Deposit')
+      .reduce((sum, t) => sum + t.amount, 0);
+  }
+
+  get totalWithdrawals(): number {
+    return this.transactions
+      .filter(t => t.type === 'Withdraw')
+      .reduce((sum, t) => sum + t.amount, 0);
+  }
+
+  get totalTransfers(): number {
+    return this.transactions
+      .filter(
+        t =>
+          t.type === 'Transfer Out' ||
+          t.type === 'Transfer In'
+      )
+      .reduce((sum, t) => sum + t.amount, 0);
+  }
+
+  get transactionCount(): number {
+    return this.transactions.length;
   }
 }
