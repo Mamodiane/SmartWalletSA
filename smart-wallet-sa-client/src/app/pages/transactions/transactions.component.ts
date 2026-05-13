8546c85106d51fde6c03daa3e24c43fe20a4b2aa
Pilatos <mamodianepilatos@gmail.com>
2026-05-13 20:10:21 +0200

    Add Angular wallet dashboard and transaction history

--- smart-wallet-sa-client/src/app/pages/transactions/transactions.component.ts
+++ smart-wallet-sa-client/src/app/pages/transactions/transactions.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TransactionService } from '../../services/transaction.service';
+import { Transaction } from '../../models/transaction.model';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
-export class TransactionsComponent {
+export class TransactionsComponent implements OnInit {
+  transactions: Transaction[] = [];
+  errorMessage = '';
 
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+
+  loadTransactions(): void {
+    this.transactionService.getTransactions().subscribe({
+      next: (response) => {
+        this.transactions = response;
+      },
+      error: () => {
+        this.errorMessage = 'Unable to load transactions.';
+      }
+    });
+  }
 }
